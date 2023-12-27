cf02fd0b92e197004539ea651869b8e238d7fbec
unknown <rifat-sarker>
2023-12-27 13:49:00 +0600

    token generate and set cookies

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l80xyen.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,12 +31,20 @@ async function run() {
     await client.connect();
 
     const blogCollection = client.db('blogDB').collection('blog')
+    const userCollection = client.db('blogDB').collection('user')
 
     app.get('/blog', async(req,res)=>{
       const cursor = blogCollection.find();
       const result = await cursor.toArray();
       res.send(result)
     })    
+
+    app.get('/blog/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await blogCollection.findOne(query)
+      res.send(result)
+    })
 
     app.post('/blog', async(req,res)=>{
       const newBlog = req.body;
@@ -41,7 +53,35 @@ async function run() {
       res.send(result)
     })
 
+    // token related apies
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.DB_TOKEN_SECRET, {expiresIn : '1hr'});
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({sucess:true})
+    })
 
+
+
+    // user related apies
+    // app.get('/user', async(req,res)=>{
+    //   const cursor = userCollection.find();
+    //   const user = await cursor.toArray();
+    //   res.send(user)
+    // })
+
+    // app.post('/user', async(req,res)=>{
+    //   const user = req.body;
+    //   console.log(user);
+    //   const result = await userCollection.insertOne(user)
+    //   res.send(result)
+
+    // })
 
 
     // Send a ping to confirm a successful connection
