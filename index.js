53a9bfb83b419271cf2c998240503bb4d0c09518
unknown <rifat-sarker>
2023-12-27 16:10:50 +0600

    jwt token verify

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -13,6 +14,7 @@ app.use(cors({
   credentials:true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l80xyen.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,15 +27,37 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// my middlware
+const logger = async(req,res,next) => {
+  console.log('called', req.host, req.originalUrl);
+  next();
+}
+
+const verifyToken = async(req,res, next)=>{
+  const token = req.cookies.token;
+  if(!token){
+    res.status(401).send({message: 'unauthorized'})
+  }
+  jwt.verify(token, process.env.DB_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      res.status(401).send({message: 'notauthorized'})
+    }
+    req.user = decoded;
+    next();
+  } )
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const blogCollection = client.db('blogDB').collection('blog')
-    const userCollection = client.db('blogDB').collection('user')
+    // const userCollection = client.db('blogDB').collection('user')
+    const wishlistCollection = client.db('blodDB').collection('wishlist')
 
-    app.get('/blog', async(req,res)=>{
+    app.get('/blog', verifyToken, async(req,res)=>{
       const cursor = blogCollection.find();
       const result = await cursor.toArray();
       res.send(result)
@@ -50,6 +74,29 @@ async function run() {
       const newBlog = req.body;
       console.log(newBlog);
       const result = await blogCollection.insertOne(newBlog);
+      res.send(result)
+    })
+
+
+    //wishlist
+    app.get('/wishlist', async(req,res)=>{
+      console.log(req.query.email);
+      // console.log('user in the valid token', req.user);
+      // console.log('tik tik token', req.cookies.token);
+      if(req.query.email != req.user.email){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await wishlistCollection.find(query).toArray();
+      res.send(result)
+    })
+    app.post('/wishlist', async(req,res)=> {
+      const wishlist = req.body;
+      console.log(wishlist);
+      const result = await wishlistCollection.insertOne(booking)
       res.send(result)
     })
 
